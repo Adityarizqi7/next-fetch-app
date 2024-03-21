88b1e8b9ddcc70725a8d37767a1b21be14aa8d3b
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2024-03-21 12:12:29 +0700

    fix: change wording at navbar & footer

--- src/app/components/header/Navigation.jsx
+++ src/app/components/header/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
             <div className="max-w-screen-xl bg-white border-b border-solid border-gray-300/80 flex flex-wrap items-center justify-between mx-auto py-6 sm:px-2 px-5">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap">Next Fetching</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap">Next Fetch App</span>
                 </Link>
                 <NavigationMobile />
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
