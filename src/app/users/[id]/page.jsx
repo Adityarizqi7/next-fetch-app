8e6afa77f55a087dc2cc72153c7e9f348538ddee
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2024-04-03 10:45:35 +0700

    chore: add seo web

--- src/app/users/[id]/page.jsx
+++ src/app/users/[id]/page.jsx
@@ -14,13 +14,62 @@ async function getData(id) {
     return res.json()
 }
 
+/** @type {import("next").Viewport} */
+export const viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 4,
+	userScalable: true,
+	shrinkToFit: 'no',
+}
+
+/** @type {import("next").Metadata} */
 export async function generateMetadata({ params }) {
 
     const { data } = await getData(params.id)
 
     return {
         title: `${data?.first_name + ' ' + data?.last_name}`,
-        description:`${data?.first_name + ' ' + data?.last_name} - API Data Fetching with Next JS by @adityarizqiardhana`,
+        description: `${data?.first_name + ' ' + data?.last_name} - API Data Fetching with Next JS by @adityarizqiardhana`,
+        applicationName: 'Next Fetch APP',
+		category: 'user detail',
+		keywords: ['Next', 'App', 'user', 'detail'],
+		authors: [{ name: 'Aditya Rizqi Ardhana', url: 'https://adityara.netlify.app' }],
+		creator: 'Aditya Rizqi Ardhana',
+		publisher: 'Aditya Rizqi Ardhana',
+		openGraph: {
+			title: `${data?.first_name + ' ' + data?.last_name}`,
+			description: `${data?.first_name + ' ' + data?.last_name} - API Data Fetching with Next JS by @adityarizqiardhana`,
+			url: `https://nextapp-fetching.netlify.app/users/${params.id}`,
+			siteName: `User Detail ${data?.first_name + ' ' + data?.last_name} - Next Fetch App`,
+			images: [
+				{
+					url: 'https://nextapp-fetching.netlify.app/src/app/opengraph-image-user-detail.png',
+					width: 800,
+					height: 600,
+				},
+			],
+			type: 'article',
+		},
+		robots: {
+			index: false,
+			follow: true,
+			nocache: true,
+			googleBot: {
+			  index: true,
+			  follow: false,
+			  noimageindex: true,
+			  'max-video-preview': -1,
+			  'max-image-preview': 'large',
+			  'max-snippet': -1,
+			},
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title: `${data?.first_name + ' ' + data?.last_name}`,
+			description: `${data?.first_name + ' ' + data?.last_name} - API Data Fetching with Next JS by @adityarizqiardhana`,
+			images: ['https://nextapp-fetching.netlify.app/src/app/opengraph-image-user-detail.png'],
+		},
     }
 }
 
